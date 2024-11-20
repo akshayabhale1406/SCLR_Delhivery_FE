c5aa182177bbdb7c84379bffb7060ba4bd430c66
Akshay Bharat Abhale <102620671+akshayabhale1406@users.noreply.github.com>
2024-11-20 23:58:34 +0530

    Update delhivery_FE.py

--- delhivery_FE.py
+++ delhivery_FE.py
@@ -3,6 +3,11 @@
 print("DELHIVERY DATA ANALYSIS")
 =======
 import pandas as pd
+import numpy as np
+import matplotlib as plt
+import seaborn as sns
+from sklearn.impute import KNNImputer
+
 
 df = pd.read_csv("/workspaces/SCLR_Delhivery_FE/delhivery_data.csv")
 df.head()
